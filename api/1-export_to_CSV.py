4ca2157fa23cd8819627ca49577fdfeea2a38762
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-22 23:02:06 +0100

    export to csv

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""a script that returns information for a given employee"""
+
+import csv
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        id_emp = argv[1]
+        url = 'https://jsonplaceholder.typicode.com/'
+        req = requests.get('{}users/{}'.format(url, id_emp)).json()
+        req_to_do = requests.get("{}todos?userID={}"
+                                 .format(url, id_emp)).json()
+        username = req.get("username")
+        with open('{}.csv'.format(id_emp), 'w', newline='') as file:
+            csv_data = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+            for task in req_to_do:
+                csv_data.writerow(["{}".format(id_emp),
+                                   "{}".format(username),
+                                   "{}".format(task.get("completed")),
+                                   "{}".format(task.get("title"))])
