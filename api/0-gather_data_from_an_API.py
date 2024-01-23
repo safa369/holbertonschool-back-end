ac97ce6faa62783215d0bfcfd7ce1a435548e133
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-23 14:33:01 +0100

    rather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
             all_tasks = len(req_to_do)
             completed_task = []
             for t_c in req_to_do:
-                if t_c.get("completed") == True:
+                if t_c.get("completed") is True:
                     completed_task.append(t_c)
             count = len(completed_task)
             print("Employee {} is done with tasks({}/{}):"
