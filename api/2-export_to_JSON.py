7fd54f565bfc6aefe47aacd7daa82a53c1614e9e
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-23 15:59:36 +0100

    export to JSON file

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""a script that returns information for a given employeein JSON format"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        id_emp = sys.argv[1]
+        url = 'https://jsonplaceholder.typicode.com/'
+        req = requests.get('{}users/{}'.format(url, id_emp)).json()
+        name_e = req.get("name")
+        username = req.get("username")
+        req_to_do = requests.get("{}todos?userID={}"
+                                 .format(url, id_emp)).json()
+        all_tasks = len(req_to_do)
+        with open('{}.json'.format(id_emp), 'w') as file:
+            json.dump({id_emp: [{
+                "task": (t_c.get("title")),
+                "completed": (t_c.get("completed")),
+                "username": username}
+                for t_c in req_to_do]}, file)
