3023835be5a94145f0e51e3e987e3a46ada8328d
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-29 13:09:39 +0100

    rather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """a script that returns information for a given employee."""
+from sys import argv
 import requests
-import sys
-
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        id_emp = sys.argv[1]
+    if len(argv) > 1:
+        id_emp = argv[1]
         url = 'https://jsonplaceholder.typicode.com/'
         req = requests.get('{}users/{}'.format(url, id_emp)).json()
         name_e = req.get("name")
         if name_e is not None:
-            req_to_do = requests.get("{}todos?userID={}"
+            req_to_do = requests.get("{}TODOS?userID={}"
                                      .format(url, id_emp)).json()
             all_tasks = len(req_to_do)
-            completed_task = []
-            for t_c in req_to_do:
-                if t_c.get("completed") is True:
-                    completed_task.append(t_c)
+            completed_task = [t_c for t_c in req_to_do if t_c.get("completed") is True]
             count = len(completed_task)
             print("Employee {} is done with tasks({}/{}):"
                   .format(name_e, count, all_tasks))
