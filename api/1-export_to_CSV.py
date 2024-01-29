61212b9db4c3e94a7464050884993586a18fecb1
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-29 15:20:54 +0100

    export data

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
         id_emp = argv[1]
         url = 'https://jsonplaceholder.typicode.com/'
         req = requests.get('{}users/{}'.format(url, id_emp)).json()
-        req_to_do = requests.get("{}todos?userID={}"
-                                 .format(url, id_emp)).json()
+        req_to_do = requests.get(url + "todos?userId={}"
+                                 .format(id_emp)).json()
         username = req.get("username")
         with open('{}.csv'.format(id_emp), 'w', newline='') as file:
             csv_data = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
