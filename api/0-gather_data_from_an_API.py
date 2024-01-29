bcaf0a14bf9a23ec70d7ca51f97eadd0e00986a1
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-29 13:11:12 +0100

    rather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """a script that returns information for a given employee."""
-from sys import argv
+
 import requests
+from sys import argv
 
 if __name__ == "__main__":
     if len(argv) > 1:
