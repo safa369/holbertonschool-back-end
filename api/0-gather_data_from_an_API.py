dc6488b7d7160075a164c0c1e0b77b8191a7ad18
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-01-29 15:15:33 +0100

    rather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """a script that returns information for a given employee."""
 
+import json
 import requests
 from sys import argv
 
 if __name__ == "__main__":
     if len(argv) > 1:
-        id_emp = argv[1]
+        id_emp = int(argv[1])
         url = 'https://jsonplaceholder.typicode.com/'
-        req = requests.get('{}users/{}'.format(url, id_emp)).json()
-        name_e = req.get("name")
-        if name_e is not None:
-            req_to_do = requests.get("{}TODOS?userID={}"
-                                     .format(url, id_emp)).json()
-            all_tasks = len(req_to_do.get("task"))
-            completed_task = [t_c for t_c in req_to_do
-                              if t_c.get("completed") is True]
-            count = len(completed_task)
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name_e, count, all_tasks))
-            for t in completed_task:
-                print("\t {}".format(t.get("title")))
+        request = requests.get('{}users/{}'.format(url, id_emp)).json()
+        name = request.get('name')
+        to_do = requests.get(url + "todos?userId={}".format(id_emp)).json()
+        completed_task = []
+        for task in to_do:
+            if task.get('completed') is True:
+                completed_task.append(task)
+        count = len(completed_task)
+        print("Employee {} is done with tasks({}/{}):"
+              .format(name, count, len(to_do)))
+        for t in completed_task:
+            print("\t {}".format(t.get("title")))
