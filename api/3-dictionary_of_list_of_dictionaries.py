9e8aed5a56c9fce8b65f20ace0b123138ca263b4
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-03-30 00:06:43 +0100

    api

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -4,6 +4,7 @@
 import json
 import requests
 
+
 def fetch_user_tasks(user_id):
     url = 'https://jsonplaceholder.typicode.com/'
     user_data = requests.get('{}users/{}'.format(url, user_id)).json()
@@ -19,10 +20,11 @@ def fetch_user_tasks(user_id):
         completed_tasks.append(task_info)
     return completed_tasks
 
+
 if __name__ == "__main__":
     all_user_tasks = {}
     for user_id in range(1, 11):  # Assuming users are from id 1 to 10
         all_user_tasks[str(user_id)] = fetch_user_tasks(user_id)
-    
+
     with open("todo_all_employees.json", "w") as json_file:
         json.dump(all_user_tasks, json_file, indent=4)
