e05e47da682ed44bbb7f088199e10d7de6205d68
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-03-30 00:04:44 +0100

    api

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""a script that returns information for a given employee."""
+"""a script that returns information for all employees."""
 
 import json
 import requests
-from sys import argv
+
+def fetch_user_tasks(user_id):
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_data = requests.get('{}users/{}'.format(url, user_id)).json()
+    username = user_data.get('username')
+    tasks = requests.get(url + "todos?userId={}".format(user_id)).json()
+    completed_tasks = []
+    for task in tasks:
+        task_info = {
+            "username": username,
+            "task": task.get("title"),
+            "completed": task.get("completed")
+        }
+        completed_tasks.append(task_info)
+    return completed_tasks
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        id_emp = int(argv[1])
-        url = 'https://jsonplaceholder.typicode.com/'
-        request = requests.get('{}users/{}'.format(url, id_emp)).json()
-        name = request.get('name')
-        to_do = requests.get(url + "todos?userId={}".format(id_emp)).json()
-        completed_tasks = []
-        for task in to_do:
-            task_info = {
-                "username": name,
-                "task": task.get("title"),
-                "completed": task.get("completed")
-            }
-            completed_tasks.append(task_info)
-        data = {str(id_emp): completed_tasks}
-        with open("todo_all_employees.json", "w") as json_file:
-            json.dump(data, json_file, indent=4)
-        print("Data exported to todo_all_employees.json successfully.")
+    all_user_tasks = {}
+    for user_id in range(1, 11):  # Assuming users are from id 1 to 10
+        all_user_tasks[str(user_id)] = fetch_user_tasks(user_id)
+    
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(all_user_tasks, json_file, indent=4)
